c198610fba74cdc86199928b2e6f7c35c41f7815
Fletcher Murray <fletchermurray@Fletchers-MacBook-Pro-2.local>
2024-07-20 13:54:08 -0600

    added tests for clearing and authorizing

---
+++ server/src/test/java/passoff/server/AuthorizationTests.java
@@ -0,0 +1,45 @@
+package passoff.server;
+
+import dataaccess.interfaces.AuthDAO;
+import dataaccess.memory.MemoryAuthDAO;
+import model.AuthData;
+import service.AuthorizationService;
+import org.junit.jupiter.api.*;
+import service.ErrorException;
+
+public class AuthorizationTests {
+    private AuthorizationService authService = new AuthorizationService();
+    private AuthDAO authDAO = new MemoryAuthDAO();
+
+    @BeforeEach
+    void setUp() {
+        authDAO.clear();
+    }
+
+    @Test
+    void authenticate()
+    {
+        try
+        {
+            authDAO.createAuth(new AuthData("1234","fletcher"));
+            authService.authorize("1234");
+        }
+        catch (Exception e)
+        {
+            Assertions.fail("didn't authorize an authorized user");
+        }
+    }
+
+    @Test
+    void notAuthenticated() {
+        try
+        {
+            authService.authorize("1234");
+            Assertions.fail("Authorized fake authToken");
+        }
+        catch (ErrorException e)
+        {
+            Assertions.assertEquals(401,e.getErrorCode());
+        }
+    }
+}
---
+++ server/src/test/java/passoff/server/ClearTests.java
@@ -0,0 +1,44 @@
+package passoff.server;
+import chess.ChessGame;
+import dataaccess.interfaces.AuthDAO;
+import dataaccess.interfaces.GameDAO;
+import dataaccess.interfaces.UserDAO;
+import dataaccess.memory.MemoryAuthDAO;
+import dataaccess.memory.MemoryGameDAO;
+import dataaccess.memory.MemoryUserDAO;
+import model.AuthData;
+import model.GameData;
+import model.UserData;
+import org.junit.jupiter.api.*;
+
+public class ClearTests {
+
+    AuthDAO authDAO = new MemoryAuthDAO();
+    GameDAO gameDAO = new MemoryGameDAO();
+    UserDAO userDAO = new MemoryUserDAO();
+
+    @Test
+    public void testClear() {
+        try {
+            AuthData authorization = new AuthData("123", "fletcher");
+            GameData game = new GameData(1, "fletcher", "emily", "test", new ChessGame());
+            UserData user = new UserData("fletcher", "1234", "fletch@email.com");
+            authDAO.createAuth(authorization);
+            gameDAO.createGame(game);
+            userDAO.createUser(user);
+
+            authDAO.clear();
+            gameDAO.clear();
+            userDAO.clear();
+
+            Assertions.assertNull(authDAO.getAuth("123"),"didn't clear authorization");
+            Assertions.assertNull(gameDAO.getGame(1),"didn't clear game");
+            Assertions.assertNull(userDAO.getUser("fletcher"),"didn't clear user");
+
+        }
+        catch (Exception e) {
+            Assertions.fail(e.getMessage());
+        }
+    }
+
+}
