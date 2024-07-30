f9e449e4b999902979c36c1fe1362fb83a0c3765
Fletcher Murray <fletchermurray@Fletchers-MBP-2.app.byu.edu>
2024-07-30 13:31:37 -0600

    added http request and response logging to server

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -3,7 +3,22 @@ package server;
 import handler.*;
 import spark.*;
 
+import java.io.IOException;
+import java.util.logging.Logger;
+import java.util.logging.Level;
+import java.util.logging.FileHandler;
+import java.util.logging.SimpleFormatter;
+
 public class Server {
+
+    private static Logger logger;
+    static {
+        try {
+            initLog();
+        } catch (IOException e) {
+            System.out.println("Error initializing log: " + e.getMessage());
+        }
+    }
 
     public int run(int desiredPort) {
         Spark.port(desiredPort);
@@ -11,6 +26,8 @@ public class Server {
         Spark.staticFiles.location("web");
 
         // Register your endpoints and handle exceptions here.
+
+        Spark.after(this::log);
 
         Spark.delete("/db", new ClearHandler());
         Spark.post("/user", new RegisterHandler());
@@ -27,5 +44,20 @@ public class Server {
     public void stop() {
         Spark.stop();
         Spark.awaitStop();
+    }
+
+    private void log(Request req, Response res) {
+        logger.log(Level.INFO, String.format("[%s]%s - %d", req.requestMethod(), req.pathInfo(), res.status()));
+    }
+
+    private static void initLog() throws IOException {
+        Level logLevel = Level.INFO;
+        logger = Logger.getLogger(Server.class.getName());
+        logger.setLevel(logLevel);
+
+        FileHandler fileHandler = new FileHandler("log.txt",false);
+        fileHandler.setLevel(logLevel);
+        fileHandler.setFormatter(new SimpleFormatter());
+        logger.addHandler(fileHandler);
     }
 }
