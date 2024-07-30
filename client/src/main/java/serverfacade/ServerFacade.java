8764b76c9e3de571999a22a0463eadc8e5098593
Fletcher Murray <fletchermurray@Fletchers-MacBook-Pro-2.local>
2024-07-30 15:42:33 -0600

    implemented ServerFacade and tests fully

--- client/src/main/java/serverfacade/ServerFacade.java
+++ client/src/main/java/serverfacade/ServerFacade.java
@@ -7,6 +7,7 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.HttpURLConnection;
+import java.net.URI;
 import java.net.URL;
 import exception.ErrorException;
 import request.*;
@@ -19,63 +20,75 @@ public class ServerFacade {
         this.serverURL = serverURL;
     }
 
-    public ClearResponse clear(ClearRequest request) throws ErrorException, IOException {
-        String path = serverURL + "/db";
+    public ClearResponse clear(ClearRequest request) throws ErrorException {
+        String path = "/db";
         String method = "DELETE";
-        try {
-            URL url = new URL(path);
-            HttpURLConnection http = (HttpURLConnection) url.openConnection();
-            http.setRequestMethod(method);
-            http.setDoOutput(true);
-
-            //write to the request body
-            if(request != null) {
-                http.setRequestProperty("Content-Type", "application/json");
-                try(OutputStream os = http.getOutputStream()) {
-                    String requestJson = new Gson().toJson(request);
-                    os.write(requestJson.getBytes());
-                }
-            }
-
-            http.connect();
-
-            //check to see if the request is successful, if not throw an error
-            if(http.getResponseCode() != HttpURLConnection.HTTP_OK) {
-                throw new ErrorException(http.getResponseCode(), "failure: " + http.getResponseMessage());
-            }
-
-            ClearResponse response = null;
-            if (http.getContentLength() < 0) {
-                try(InputStream is = http.getInputStream()) {
-                    response = new Gson().fromJson(new InputStreamReader(is), ClearResponse.class);
-                }
-            }
-
-            return response;
-        }
-        catch (Exception e) {
-            throw new ErrorException(500,e.getMessage());
-        }
+        return makeRequest(method,path,request,null,ClearResponse.class);
     }
 
-    /*
-     private <T> T makeRequest(String method, String path, Object request, Class<T> responseType) throws ErrorException {
+    public RegisterResponse register(RegisterRequest request) throws ErrorException {
+        String path = "/user";
+        String method = "POST";
+        return makeRequest(method,path,request,null,RegisterResponse.class);
+    }
+
+    public LoginResponse login(LoginRequest request) throws ErrorException {
+        String path = "/session";
+        String method = "POST";
+        return makeRequest(method,path,request,null,LoginResponse.class);
+    }
+
+    public CreateGameResponse createGame(CreateGameRequest request,String authToken) throws ErrorException {
+        String path = "/game";
+        String method = "POST";
+        return makeRequest(method,path,request,authToken,CreateGameResponse.class);
+    }
+
+    public LogoutResponse logout(LogoutRequest request,String authToken) throws ErrorException {
+        String path = "/session";
+        String method = "DELETE";
+        return makeRequest(method,path,request,authToken,LogoutResponse.class);
+    }
+
+    public ListGamesResponse listGames(ListGamesRequest request, String authToken) throws ErrorException {
+        String path = "/game";
+        String method = "GET";
+        return makeRequest(method,path,null,authToken,ListGamesResponse.class);
+    }
+
+    public JoinGameResponse joinGame(JoinGameRequest request, String authToken) throws ErrorException {
+        String path = "/game";
+        String method = "PUT";
+        return makeRequest(method,path,request,authToken,JoinGameResponse.class);
+    }
+
+
+     private <T> T makeRequest(String method, String path, Object request, String authToken, Class<T> responseType) throws ErrorException {
         try {
-            URL url = new URL(serverURL + path);
+            URL url = (new URI(serverURL + path)).toURL();
             HttpURLConnection http = (HttpURLConnection) url.openConnection();
             http.setRequestMethod(method);
+            if (authToken != null) {
+                http.setRequestProperty("authorization", authToken);
+            }
             http.setDoOutput(true);
+
+            //writes the request body and authToken into the http request
             writeBody(request,http);
+            //sends the request to the server
             http.connect();
             throwIfNotSuccessful(http);
-            return new T;
+            //reads the response body from the http response
+            return readBody(http,responseType);
+        }
+        catch (ErrorException e) {
+            throw e;
         }
         catch (Exception e) {
             throw new ErrorException(500,e.getMessage());
         }
 
     }
-     */
 
 
     private void writeBody(Object request, HttpURLConnection http) throws IOException {
@@ -88,13 +101,24 @@ public class ServerFacade {
         }
     }
 
+    private <T> T readBody(HttpURLConnection http, Class<T> responseType) throws IOException {
+        T response = null;
+        if(http.getContentLength() < 0) {
+            try (InputStream responseBody = http.getInputStream()) {
+                InputStreamReader reader = new InputStreamReader(responseBody);
+                if(responseType != null) {
+                    response = new Gson().fromJson(reader, responseType);
+                }
+            }
+        }
+        return response;
+    }
+
     private void throwIfNotSuccessful(HttpURLConnection http) throws IOException, ErrorException {
         int status = http.getResponseCode();
         boolean successful = status / 100 == 2;
         if(!successful) {
             throw new ErrorException(status,"failure " + status);
         }
-
     }
-
 }
--- client/src/test/java/client/ServerFacadeTests.java
+++ client/src/test/java/client/ServerFacadeTests.java
@@ -1,5 +1,7 @@
 package client;
 
+import chess.ChessGame;
+import exception.ErrorException;
 import org.junit.jupiter.api.*;
 import request.*;
 import response.*;
@@ -11,6 +13,7 @@ public class ServerFacadeTests {
 
     private static Server server;
     private static ServerFacade facade;
+    String authToken;
 
     @BeforeAll
     public static void init() {
@@ -23,6 +26,21 @@ public class ServerFacadeTests {
     @AfterAll
     static void stopServer() {
         server.stop();
+    }
+
+    @AfterEach
+    public void cleanUp() {
+        try {
+            facade.clear(new ClearRequest());
+        } catch (ErrorException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    private String registerDefaultUser() throws ErrorException {
+        RegisterRequest request = new RegisterRequest("fletchmur","123","fletch@email.com");
+        RegisterResponse response = facade.register(request);
+        return response.authToken();
     }
 
 
@@ -41,6 +59,231 @@ public class ServerFacadeTests {
             Assertions.fail(e.getMessage());
         }
 
+    }
+
+    @Test
+    public void createGameRequest() {
+        try {
+            String authToken = registerDefaultUser();
+            CreateGameRequest request = new CreateGameRequest("testGame");
+            CreateGameResponse response = facade.createGame(request,authToken);
+            Assertions.assertInstanceOf(CreateGameResponse.class,response);
+            Assertions.assertNotNull(response.gameID());
+        }
+        catch (Exception e) {
+            Assertions.fail(e.getMessage());
+        }
+    }
+
+    @Test
+    public void createGameBadRequest() {
+        try {
+            String authToken = registerDefaultUser();
+            CreateGameRequest request = new CreateGameRequest(null);
+            facade.createGame(request,authToken);
+        }
+        catch (ErrorException e) {
+            Assertions.assertEquals(400,e.getErrorCode());
+        }
+    }
+
+    @Test
+    public void createGameNotAuthorized() {
+        try {
+            String authToken = "abc";
+            CreateGameRequest request = new CreateGameRequest("testGame");
+            facade.createGame(request,authToken);
+            Assertions.fail("created game when unauthorized");
+        }
+        catch (ErrorException e) {
+            Assertions.assertEquals(401,e.getErrorCode());
+        }
+    }
+
+    @Test
+    public void registerUser() {
+        try {
+            RegisterRequest request = new RegisterRequest("fletchmur","123","fletch@email.com");
+            RegisterResponse response = facade.register(request);
+            Assertions.assertInstanceOf(RegisterResponse.class,response);
+            Assertions.assertEquals("fletchmur", response.username());
+            Assertions.assertNotNull(response.authToken());
+        }
+        catch (Exception e) {
+            Assertions.fail(e.getMessage());
+        }
+    }
+
+    @Test
+    void registerUserTwice() {
+        try {
+            RegisterRequest request = new RegisterRequest("fletchmur","123","fletch@email.com");
+            facade.register(request);
+            facade.register(request);
+            Assertions.fail("registered user twice");
+        }
+        catch (ErrorException e) {
+            Assertions.assertEquals(403,e.getErrorCode());
+        }
+    }
+
+    @Test
+    void logout() {
+        try {
+            RegisterRequest registerRequest = new RegisterRequest("fletchmur","123","fletch@email.com");
+            String authToken = facade.register(registerRequest).authToken();
+            LogoutResponse response = facade.logout(new LogoutRequest(),authToken);
+
+            Assertions.assertInstanceOf(LogoutResponse.class,response);
+        }
+        catch (ErrorException e) {
+            Assertions.fail(e.getMessage());
+        }
+    }
+
+    @Test
+    void logoutUnauthorized() {
+        try {
+            facade.logout(new LogoutRequest(), "fake token");
+            Assertions.fail("logout unauthorized");
+        }
+        catch(ErrorException e) {
+            Assertions.assertEquals(401,e.getErrorCode());
+        }
+    }
+
+    @Test
+    void login() {
+        try {
+            String authToken = registerDefaultUser();
+            facade.logout(new LogoutRequest(),authToken);
+
+            LoginRequest request = new LoginRequest("fletchmur","123");
+            LoginResponse response = facade.login(request);
+            Assertions.assertInstanceOf(LoginResponse.class,response);
+            Assertions.assertEquals("fletchmur", response.username());
+            Assertions.assertNotNull(response.authToken());
+
+        }
+        catch (ErrorException e) {
+            Assertions.fail(e.getMessage());
+        }
+    }
+
+    @Test
+    void loginFakeUser() {
+        try {
+            LoginRequest request = new LoginRequest("fletchmur","123");
+            LoginResponse response = facade.login(request);
+            Assertions.fail("login fake user");
+
+        }
+        catch (ErrorException e) {
+           Assertions.assertEquals(401,e.getErrorCode());
+        }
+    }
+
+    @Test
+    void getAllGames() {
+        try {
+            String authToken = registerDefaultUser();
+            CreateGameRequest request1 = new CreateGameRequest("testGame1");
+            CreateGameRequest request2 = new CreateGameRequest("testGame2");
+            facade.createGame(request1,authToken);
+            facade.createGame(request2,authToken);
+
+            ListGamesResponse response = facade.listGames(new ListGamesRequest(),authToken);
+            Assertions.assertInstanceOf(ListGamesResponse.class,response);
+            Assertions.assertEquals(2,response.games().length);
+
+        }
+        catch (ErrorException e) {
+            Assertions.fail(e.getMessage());
+        }
+    }
+
+    @Test
+    void getAllGamesUnauthorized() {
+        try {
+            String authToken = registerDefaultUser();
+            CreateGameRequest request1 = new CreateGameRequest("testGame1");
+            CreateGameRequest request2 = new CreateGameRequest("testGame2");
+            facade.createGame(request1,authToken);
+            facade.createGame(request2,authToken);
+
+            facade.logout(new LogoutRequest(),authToken);
+
+            ListGamesResponse response = facade.listGames(new ListGamesRequest(),authToken);
+
+
+        }
+        catch (ErrorException e) {
+            Assertions.assertEquals(401,e.getErrorCode());
+        }
+    }
+
+    @Test
+    void joinGameWhite() {
+        try {
+            String authToken = registerDefaultUser();
+            CreateGameRequest createGameRequest = new CreateGameRequest("testGame");
+            CreateGameResponse createGameResponse = facade.createGame(createGameRequest,authToken);
+            JoinGameRequest request = new JoinGameRequest(ChessGame.TeamColor.WHITE,createGameResponse.gameID());
+            JoinGameResponse response = facade.joinGame(request,authToken);
+            Assertions.assertInstanceOf(JoinGameResponse.class,response);
+        }
+        catch (ErrorException e) {
+            Assertions.fail(e.getMessage());
+        }
+    }
+
+    @Test
+    void joinGameBlack() {
+        try {
+            String authToken = registerDefaultUser();
+            CreateGameRequest createGameRequest = new CreateGameRequest("testGame");
+            CreateGameResponse createGameResponse = facade.createGame(createGameRequest,authToken);
+            JoinGameRequest request = new JoinGameRequest(ChessGame.TeamColor.BLACK,createGameResponse.gameID());
+            JoinGameResponse response = facade.joinGame(request,authToken);
+            Assertions.assertInstanceOf(JoinGameResponse.class,response);
+        }
+        catch (ErrorException e) {
+            Assertions.fail(e.getMessage());
+        }
+    }
+
+    @Test
+    void joinGameWhiteTaken() {
+        try {
+            String authToken = registerDefaultUser();
+            CreateGameRequest createGameRequest = new CreateGameRequest("testGame");
+            CreateGameResponse createGameResponse = facade.createGame(createGameRequest,authToken);
+
+            JoinGameRequest request = new JoinGameRequest(ChessGame.TeamColor.WHITE,createGameResponse.gameID());
+             facade.joinGame(request,authToken);
+             facade.joinGame(request,authToken);
+             Assertions.fail("joined white team but it was already taken");
+        }
+        catch (ErrorException e) {
+            Assertions.assertEquals(403,e.getErrorCode());
+        }
+    }
+
+    @Test
+    void joinGameBlackTaken() {
+        try {
+            String authToken = registerDefaultUser();
+            CreateGameRequest createGameRequest = new CreateGameRequest("testGame");
+            CreateGameResponse createGameResponse = facade.createGame(createGameRequest,authToken);
+
+            JoinGameRequest request = new JoinGameRequest(ChessGame.TeamColor.BLACK,createGameResponse.gameID());
+            facade.joinGame(request,authToken);
+            facade.joinGame(request,authToken);
+            Assertions.fail("joined white team but it was already taken");
+        }
+        catch (ErrorException e) {
+            Assertions.assertEquals(403,e.getErrorCode());
+        }
     }
 
 }
